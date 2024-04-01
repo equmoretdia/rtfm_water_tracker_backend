b1e7951247a6427e2a5376f2d0aa282fdd37c6ac
Yevheniia <Veintq2291244@gmail.com>
2024-04-01 22:28:38 +0300

    user-settings-1

--- app.js
+++ app.js
@@ -8,16 +8,19 @@ dotenv.config();
 import { authRouter } from "./routes/authRouter.js";
 import waterRateRouter from "./routes/waterRateRoutes.js";
 import { waterRouter } from "./routes/waterRouter.js";
+import userSettingsRoutes from "./routes/userSettingsRoutes.js";
 
 export const app = express();
 
 app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 app.use("/api/user", waterRateRouter);
 app.use("/api/water", waterRouter);
+app.use("/api/user-settings", userSettingsRoutes);
 
 app.use((_, res) => {
   res.status(404).json({ message: "Route not found" });
